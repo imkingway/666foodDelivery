554025f375834a06cd64a7fe6f0d5add44b19186
pjiale <o-jiale.phea@beyondsoft.com>
2021-03-06 13:35:15 +0800

    aadmin

---
+++ 666foodDelivery/Areas/Identity/Data/_666foodDeliveryUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace _666foodDelivery.Areas.Identity.Data
+{
+    // Add profile data for application users by adding properties to the _666foodDeliveryUser class
+    public class _666foodDeliveryUser : IdentityUser
+    {
+    }
+}
