38ec17bc2e93764e3f782ed1391604c92b10b3de
pjiale <o-jiale.phea@beyondsoft.com>
2021-03-14 15:37:12 +0800

    edit food function

--- 666foodDelivery/Controllers/FoodsController.cs
+++ 666foodDelivery/Controllers/FoodsController.cs
@@ -102,6 +102,15 @@ namespace _666foodDelivery.Views.Foods
             {
                 try
                 {
+                    //delete old image
+                    var foods = await _context.Food.AsNoTracking().FirstOrDefaultAsync(m => m.ID == id);
+                    CloudBlobContainer container = GetCloudBlobContainer();
+                    string blobURL = foods.BlobURL;
+                    var temp = new Uri(blobURL).LocalPath;
+                    string filename = temp.Remove(0, temp.IndexOf('/', 1) + 1);
+                    CloudBlockBlob blobs = container.GetBlockBlobReference(filename);
+                    await blobs.DeleteIfExistsAsync();
+                    
                     _context.Update(food);
                     await _context.SaveChangesAsync();
                 }
@@ -192,22 +201,13 @@ namespace _666foodDelivery.Views.Foods
             CloudBlockBlob blob = cloud.GetBlockBlobReference(files.FileName);
 
             int id = int.Parse(Request.Form["imageProductId"]);
-            var foods = await _context.Food
-                .FirstOrDefaultAsync(m => m.ID == id);
+            var foods = await _context.Food.FirstOrDefaultAsync(m => m.ID == id);
             food.ID = foods.ID;
             food.FoodName = foods.FoodName;
             food.FoodProducedDate = foods.FoodProducedDate;
             food.Type = foods.Type;
             food.Price = foods.Price;
             food.Ingredient = foods.Ingredient;
-
-            //delete old image
-            CloudBlobContainer container = GetCloudBlobContainer();
-            string blobURL = foods.BlobURL;
-            var temp = new Uri(blobURL).LocalPath;
-            string filename = temp.Remove(0, temp.IndexOf('/', 1) + 1);
-            CloudBlockBlob blobs = container.GetBlockBlobReference(filename);
-            await blobs.DeleteAsync();
 
             //upload new image
             await blob.UploadFromStreamAsync(files.OpenReadStream());
