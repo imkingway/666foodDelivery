16347f8cfbce7de9a0ad032e96cf9d5178bb9891
pjiale <o-jiale.phea@beyondsoft.com>
2021-03-14 13:49:56 +0800

    edit and delete food function

--- 666foodDelivery/Controllers/FoodsController.cs
+++ 666foodDelivery/Controllers/FoodsController.cs
@@ -145,6 +145,14 @@ namespace _666foodDelivery.Views.Foods
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
             var food = await _context.Food.FindAsync(id);
+
+            CloudBlobContainer container = GetCloudBlobContainer();
+            string blobURL = food.BlobURL;
+            var temp = new Uri(blobURL).LocalPath;
+            string filename = temp.Remove(0, temp.IndexOf('/', 1) + 1);
+            CloudBlockBlob blob = container.GetBlockBlobReference(filename);
+            await blob.DeleteAsync();
+
             _context.Food.Remove(food);
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
@@ -176,6 +184,38 @@ namespace _666foodDelivery.Views.Foods
             ViewData["BlobUrl"] = blobUrl;
 
             return View("Create");
+        }
+
+        public async Task<IActionResult> EditImage(IFormFile files, Food food)
+        {
+            CloudBlobContainer cloud = GetCloudBlobContainer();
+            CloudBlockBlob blob = cloud.GetBlockBlobReference(files.FileName);
+
+            int id = int.Parse(Request.Form["imageProductId"]);
+            var foods = await _context.Food
+                .FirstOrDefaultAsync(m => m.ID == id);
+            food.ID = foods.ID;
+            food.FoodName = foods.FoodName;
+            food.FoodProducedDate = foods.FoodProducedDate;
+            food.Type = foods.Type;
+            food.Price = foods.Price;
+            food.Ingredient = foods.Ingredient;
+
+            //delete old image
+            CloudBlobContainer container = GetCloudBlobContainer();
+            string blobURL = foods.BlobURL;
+            var temp = new Uri(blobURL).LocalPath;
+            string filename = temp.Remove(0, temp.IndexOf('/', 1) + 1);
+            CloudBlockBlob blobs = container.GetBlockBlobReference(filename);
+            await blobs.DeleteAsync();
+
+            //upload new image
+            await blob.UploadFromStreamAsync(files.OpenReadStream());
+
+            var blobUrl = blob.Uri.AbsoluteUri;
+            ViewData["BlobUrl"] = blobUrl;
+
+            return View("Edit", food);
         }
     }
 }
