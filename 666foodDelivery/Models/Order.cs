4d8eae14d30f08d583a4d80bc172519aeefc2ac8
kingway <tp046721@mail.apu.edu.my>
2021-03-20 21:08:46 +0800

    add service bus

--- 666foodDelivery/Models/Order.cs
+++ 666foodDelivery/Models/Order.cs
@@ -8,19 +8,21 @@ namespace _666foodDelivery.Models
 {
     public class Order
     {
-
-        public int ID { get; set; }
         [Required]
         [Display(Name = "Name")]
         public string CustomerName { get; set; }
+
         [Required]
         public string Address { get; set; }
+
         [Required]
         [Display(Name = "Phone Number")]
         public string PhoneNumber { get; set; }
+
         [Required]
         [Display(Name = "Food Name")]
         public string FoodName { get; set; }
+
         [Required]
         [Display(Name = "Delivery Time")]
         public DateTime DeliverTime { get; set; }
@@ -29,12 +31,5 @@ namespace _666foodDelivery.Models
         [Range(1, int.MaxValue, ErrorMessage = "The order must be more than 1")]
         [Display(Name = "Number of Order")]
         public int quantity { get; set; }
-
-        [Display(Name = "Notes")]
-        public string notes { get; set; }
-
-
-
-
     }
 }
